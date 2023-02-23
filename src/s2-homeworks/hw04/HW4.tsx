cab3a530c15c442241dccf08a5dfd20b8d9687be
MariaShev777 <mariashevtsova1403@gmail.com>
2023-02-23 22:49:31 +0300

    css amendments

--- src/s2-homeworks/hw04/HW4.tsx
+++ src/s2-homeworks/hw04/HW4.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import s2 from '../../s1-main/App.module.css'
 import Stand from './Stand'
+import SuperButton from "./common/c2-SuperButton/SuperButton";
 
 /*
      * 1 - понять (и простить) SuperInputText ++
@@ -13,7 +14,6 @@ const HW4 = () => {
     return (
         <div id={'hw4'}>
             <div className={s2.hwTitle}>Homework №4</div>
-            {/*демонстрация возможностей компонент:*/}
             <div className={s2.hw}>
                 <Stand />
             </div>
