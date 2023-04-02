4ec3ebe59f1c202a3fb09058ad761fd34640720b
MariaShev777 <mariashevtsova1403@gmail.com>
2023-04-02 16:51:10 +0300

    8HW - done

--- src/s2-homeworks/hw08/HW8.tsx
+++ src/s2-homeworks/hw08/HW8.tsx
@@ -6,9 +6,9 @@ import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
 import User from './User'
 
 /*
-* 1 - дописать типы и логику (сортировка по имени, фильтрация по совершеннолетию) homeWorkReducer, проверить тестом
-* 2 - дописать компоненту User
-* 3 - сделать стили в соответствии с дизайном
+      * 1 - дописать типы и логику (сортировка по имени, фильтрация по совершеннолетию) homeWorkReducer, проверить тестом
+      * 2 - дописать компоненту User
+      * 3 - сделать стили в соответствии с дизайном
 * */
 
 export type UserType = {
@@ -20,10 +20,10 @@ export type UserType = {
 const initialPeople: UserType[] = [
     // студенты могут поменять имя/возраст/количество объектов, _id должны быть целочисленные
     {_id: 0, name: 'Кот', age: 3},
-    {_id: 1, name: 'Александр', age: 66},
+    {_id: 1, name: 'Дмитрий', age: 66},
     {_id: 2, name: 'Коля', age: 16},
     {_id: 3, name: 'Виктор', age: 19},
-    {_id: 4, name: 'Дмитрий', age: 40},
+    {_id: 4, name: 'Антонио', age: 35},
     {_id: 5, name: 'Ирина', age: 55},
 ]
 
@@ -55,28 +55,28 @@ const HW8 = () => {
 
     return (
         <div id={'hw3'}>
-            <div className={s2.hwTitle}>Homework #8</div>
+            <div className={s2.hwTitle}>Homework №8</div>
             <div className={s2.hw}>
                 <div className={s.container}>
                     <div className={s.buttonsContainer}>
                         <SuperButton
                             id={'hw8-button-up'}
                             onClick={sortUp}
-                            xType={currentSort === 'up' ? '' : 'secondary'}
+                            xType={currentSort === 'up' ? 'default' : 'secondary'}
                         >
                             Sort up
                         </SuperButton>
                         <SuperButton
                             id={'hw8-button-down'}
                             onClick={sortDown}
-                            xType={currentSort === 'down' ? '' : 'secondary'}
+                            xType={currentSort === 'down' ? 'default' : 'secondary'}
                         >
                             Sort down
                         </SuperButton>
                         <SuperButton
                             id={'hw8-button-18'}
                             onClick={check18}
-                            xType={currentSort === '18' ? '' : 'secondary'}
+                            xType={currentSort === '18' ? 'default' : 'secondary'}
                         >
                             Check 18+
                         </SuperButton>
@@ -85,7 +85,7 @@ const HW8 = () => {
                     <table id={'hw8-users'} className={s.users}>
                         <thead className={s.thead}>
                         <tr>
-                            <td className={s.nameCol}>Full Name</td>
+                            <td className={s.nameCol}>Name</td>
                             <td className={s.ageCol}>Age</td>
                         </tr>
                         </thead>
--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -2,23 +2,22 @@ import {UserType} from '../HW8'
 
 type ActionType = | { type: 'sort'; payload: 'up' | 'down' } | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: UserType[], action: ActionType): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
-        // case 'sort': { // by name
-            // return [...state.sort((a, b) => a - b)]
-        // }
-
-
-
-
-        case 'check': {
-
-            return state.filter(el=> el.age === action.payload) // need to fix
+        case 'sort': { // by name
+            if (action.payload === 'up') {
+                return [...state.sort((a, b) => a.name > b.name ? 1 : -1)]
+            } else {
+                return [...state.sort((a, b) => a.name > b.name ? -1 : 1)]
+            }
         }
 
 
 
 
+        case 'check': {
+            return state.filter(el => el.age >= action.payload)
+        }
         default:
             return state
     }
